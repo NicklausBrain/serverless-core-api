da900d69b7edbe0d8d121dbfbf32f3cca7428d4a
Nikolay Mozgovoy <nikolay.mozgovoy@sigma.software>
2020-08-25 20:37:50 +0300

    ServerLessHostEnvironment

---
+++ cs/FuncApp/ServerLessHostEnvironment.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.FileProviders;
+
+namespace FuncApp
+{
+	public class ServerLessHostEnvironment : IWebHostEnvironment
+	{
+		public ServerLessHostEnvironment(ExecutionContext context)
+		{
+			WebRootPath = context.FunctionAppDirectory;
+			ContentRootPath = context.FunctionAppDirectory;
+		}
+
+		public string EnvironmentName { get; set; }
+		public string ApplicationName { get; set; }
+		public string ContentRootPath { get; set; }
+		public string WebRootPath { get; set; }
+		public IFileProvider ContentRootFileProvider { get; set; }
+		public IFileProvider WebRootFileProvider { get; set; }
+	}
+}
